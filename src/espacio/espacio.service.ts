091cf78c6fa711ca154377ba275f92a7e9cd085a
Heber Miguel Ramirez <heber12mig@gmail.com>
2024-10-20 01:27:10 -0600

    fix-all-error-espacios-comillas

--- src/espacio/espacio.service.ts
+++ src/espacio/espacio.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+//import { InjectModel } from '@nestjs/mongoose';
+//import { Model } from 'mongoose';
+//import { Space, SpaceDocument } from 'src/schemas/space.schema';
 
 @Injectable()
 export class EspacioService {}
+//async createSpace(spaceData: any <Space>: Promise<Space>){
+// const newSpace = new this.spaceModel(spaceData);
+//return await newSpace.save();
--- src/schemas/space.schema.ts
+++ src/schemas/space.schema.ts
@@ -1,29 +1,43 @@
-import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 export type SpaceDocument = Space & Document;
 
 @Schema({
-    timestamps: true,
+  timestamps: true,
 })
 export class Space {
-    @Prop({
-        required: true,
-        trim: true
-    })
-    name: string;
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  name: string;
 
-    @Prop({
-        required: true,
-        trim: true
-    })
-    location: string;
-    
-    @Prop({
-        required: true,
-        min: 1
-    })
-    capacity: number;
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  description: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+    default: true,
+  })
+  availability: boolean;
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  location: string;
+
+  @Prop({
+    required: true,
+    min: 1,
+    max: 100,
+  })
+  capacity: number;
 }
 
 export const SpaceSchema = SchemaFactory.createForClass(Space);
